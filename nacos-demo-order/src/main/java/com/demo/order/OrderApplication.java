86e9576e51ca15e8c8e769b2381f46dffcf4654a
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2023-07-23 11:16:50 +0800

    第一次递交

--- nacos-demo-order/src/main/java/com/demo/order/OrderApplication.java
+++ nacos-demo-order/src/main/java/com/demo/order/OrderApplication.java
@@ -10,6 +10,4 @@ public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
-
-    
 }
--- nacos-demo-order/src/main/java/com/demo/order/controller/OrderController.java
+++ nacos-demo-order/src/main/java/com/demo/order/controller/OrderController.java
@@ -2,15 +2,30 @@ package com.demo.order.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${a.b}")
+    private String dateformate;
+
+    @GetMapping("now")
+    public String now() {
+        log.info("createOrder请求入参:{}");
+        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformate));
+        log.info("dateTime:{}",dateTime);
+        return "验证配置的日期: " + dateTime;
+    }
 
     @GetMapping("/order/create")
     public String createOrder(Integer productId, Integer userId) {
